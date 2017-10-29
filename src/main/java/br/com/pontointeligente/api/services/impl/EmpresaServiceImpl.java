3788a7ddfddc4032ccc58d471f91b090fa4510c2
Dionatan Ribeiro <dionatangil@gmail.com>
2017-10-29 10:20:25 -0200

    fix empresaServic

--- src/main/java/br/com/pontointeligente/api/services/impl/EmpresaServiceImpl.java
+++ src/main/java/br/com/pontointeligente/api/services/impl/EmpresaServiceImpl.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.pontointeligente.api.entities.Empresa;
 import br.com.pontointeligente.api.repositories.EmpresaRepository;
 import br.com.pontointeligente.api.services.EmpresaService;
 
+@Service
 public class EmpresaServiceImpl implements EmpresaService {
 
 	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
