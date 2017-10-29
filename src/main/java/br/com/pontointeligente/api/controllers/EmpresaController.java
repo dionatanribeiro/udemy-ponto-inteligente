6c078665f18a73a4321f2b979ba79b4dbbeea493
Dionatan Ribeiro <dionatangil@gmail.com>
2017-10-29 14:40:46 -0200

    adicionar controller de empresas

---
+++ src/main/java/br/com/pontointeligente/api/controllers/EmpresaController.java
@@ -0,0 +1,67 @@
+package br.com.pontointeligente.api.controllers;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pontointeligente.api.dtos.EmpresaDto;
+import br.com.pontointeligente.api.entities.Empresa;
+import br.com.pontointeligente.api.response.Response;
+import br.com.pontointeligente.api.services.EmpresaService;
+
+@RestController
+@RequestMapping("/api/empresas")
+@CrossOrigin("*")
+public class EmpresaController {
+
+	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	/**
+	 * Retorna uma empresa dado um CNPJ
+	 * 
+	 * @param cnpj
+	 * @return
+	 */
+	@GetMapping(value = "/cnpj/{cnpj}")
+	public ResponseEntity<Response<EmpresaDto>> buscarPorCnpj(@PathVariable("cnpj") String cnpj) {
+		log.info("Buscando empresas por CNPJ: {}", cnpj);
+		
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		Optional<Empresa> empresa = empresaService.buscarPorCnpj(cnpj);
+		
+		if (!empresa.isPresent()) {
+			log.info("Empresa não encontrada para o CNPJ: {}", cnpj);
+			response.getErrors().add("Empresa não encontrada para o CNPJ: " + cnpj);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(this.converterParaEmpresaDto(empresa.get()));
+		return ResponseEntity.ok(response);
+	}
+
+	/**
+	 * Popula um DTO com os dados de uma empresa.
+	 * 
+	 * @param empresa
+	 * @return
+	 */
+	private EmpresaDto converterParaEmpresaDto(Empresa empresa) {
+		EmpresaDto empresaDto = new EmpresaDto();
+		empresaDto.setId(empresa.getId());
+		empresaDto.setCnpj(empresa.getCnpj());
+		empresaDto.setRazaoSocial(empresa.getRazaoSocial());
+		return empresaDto;
+	}
+	
+}
---
+++ src/test/java/br/com/pontointeligente/api/controllers/EmpresaControllerTest.java
@@ -0,0 +1,75 @@
+package br.com.pontointeligente.api.controllers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import br.com.pontointeligente.api.entities.Empresa;
+import br.com.pontointeligente.api.services.EmpresaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class EmpresaControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private EmpresaService empresaService;
+	
+	private static final String BUSCAR_EMPRESA_CNPJ_URL = "/api/empresas/cnpj/";
+	private static final Long ID = Long.valueOf(1);
+	private static final String CNPJ = "08025544000110";
+	private static final String RAZAO_SOCIAL = "Empresa XYZ";
+	
+	@Test
+	public void testBuscarEmpresaCnpjInvalido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString()))
+			.willReturn(Optional.empty());
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ).accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.errors").value("Empresa não encontrada para o CNPJ: " + CNPJ));
+	}
+
+	@Test
+	public void testBuscarEmpresaCnpjValido() throws Exception {
+		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString()))
+			.willReturn(Optional.of(this.obterDadosEmpresa()));
+		
+		mvc.perform(MockMvcRequestBuilders.get(BUSCAR_EMPRESA_CNPJ_URL + CNPJ)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.id").value(ID))
+			.andExpect(jsonPath("$.data.razaoSocial", CoreMatchers.equalTo(RAZAO_SOCIAL)))
+			.andExpect(jsonPath("$.data.cnpj", CoreMatchers.equalTo(CNPJ)))
+			.andExpect(jsonPath("$.errors").isEmpty());
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setId(ID);
+		empresa.setCnpj(CNPJ);
+		empresa.setRazaoSocial(RAZAO_SOCIAL);
+		return empresa;
+	}
+	
+}
